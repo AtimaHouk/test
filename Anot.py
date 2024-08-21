66925e6c489bac9bbb4fc6a058245acf24f2ea53
Tsimur <houkatima@gmail.com>
2024-08-21 16:40:58 +0200

    a2

--- Anot.py
+++ Anot.py
@@ -2,19 +2,23 @@ tasklist = {}
 number = 1
 ammount = 0
 inf = 0
-def create():
-    t = f"{number}tnumb"
-    tasklist[t] = number
-    t = f"{number}tname"
-    tasklist[t] = input("Enter your tasks name: ")
-    t = f"{number}tdesc"
-    tasklist[t] = input("Enter your tasks description: ")
-    t = f"{number}tprio"
-    tasklist[t] = float(input("Enter your tasks priority: "))
-    number += 1
-    ammount += 1
-
 
 while inf == 0:
-    command = input("Select your action:\ncreate task\ndelete task\nedit task\nshow tasks\nedit filters")
-    
+    command = input("Select your action:\n  create task\n  delete task\n  edit task\n  show tasks\n  edit filters\n")
+    if command == "create task":
+        tasklist[number] = number
+        t = f"{number}tname"
+        tasklist[t] = input("Enter your task's name: ")
+        t = f"{number}tdesc"
+        tasklist[t] = input("Enter your task's description: ")
+        t = f"{number}tprio"
+        tasklist[t] = int(input("Enter your task's priority: "))
+        print(f"Created task number {number}, {tasklist[f'{number}tname']}, {tasklist[f'{number}tprio']}: {tasklist[f'{number}tdesc']}")
+        number += 1
+        ammount += 1
+    elif command == "delete task":
+        command = input("Enter the number of task what you want delete: ")
+        del tasklist[f"{command}tname"]
+        del tasklist[command]
+        del tasklist[f"{command}tdesc"]
+        del tasklist[f"{command}tprio"]
